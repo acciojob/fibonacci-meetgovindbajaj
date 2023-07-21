f5816f3a85460745f611be3f8853139d6e673590
Govind Bajaj <meetgovindbajaj@gmail.com>
2023-07-21 11:28:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function fibonacci(num) {
 // your code here
 	return fib1(num);
 }
-function fib1(int n) {
-        if (n <= 0)
+function fib1(n) {
+        if (n <== 0)
             return 0;
         return recursive(n - 1, 0, 1);
     }
-function recursive(int n, int prev, int curr) {
-        if (n == 0)
+function recursive(n, prev, curr) {
+        if (n === 0)
             return curr;
         return recursive(n - 1, curr, curr + prev);
     }
